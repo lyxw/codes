68410fe0c8dcca219733e3ac49abc7d5527807be
lyxw <lyxw@users.noreply.github.com>
2018-04-14 13:46:22 +0800

    Python调用Win32 API实现截图

--- tools/PrintScreen.py
+++ tools/PrintScreen.py
@@ -47,7 +47,7 @@ def printScreen(filename):
         win32api.keybd_event(0x91, 0, win32con.KEYEVENTF_KEYUP, 0)    # https://msdn.microsoft.com/en-us/library/windows/desktop/ms646304(v=vs.85).aspx
         win32api.keybd_event(0x2C, 0, win32con.KEYEVENTF_KEYUP, 0)
     except:
-        print 'keyboard event does not successful.'
+        print('keyboard event does not successful.')
         sys.exit(1)
     
     BI_BITFIELDS = 3
@@ -57,7 +57,7 @@ def printScreen(filename):
         if win32clipboard.IsClipboardFormatAvailable(win32clipboard.CF_DIB):    # https://msdn.microsoft.com/en-us/library/windows/desktop/ms649013(v=vs.85).aspx
             data = win32clipboard.GetClipboardData(win32clipboard.CF_DIB)
         else:
-            print 'cliboard does not contain an image in DIB format.'
+            print('cliboard does not contain an image in DIB format.')
             sys.exit(1)
     finally:
         win32clipboard.CloseClipboard()
@@ -66,7 +66,7 @@ def printScreen(filename):
     memmove(pointer(BitMapInfoHeaderHandle), data, SIZEOF_BITMAPINFOHEADER)
     
     if BitMapInfoHeaderHandle.biCompression != BI_BITFIELDS:
-        print 'insupported compression type {}'.format(BitMapInfoHeaderHandle.biCompression)
+        print('insupported compression type {}'.format(BitMapInfoHeaderHandle.biCompression))
         sys.exit(1)
         
     BitMapFileHeaderHandle = BITMAPFILEHEADER()
@@ -79,4 +79,7 @@ def printScreen(filename):
     with open(filename, 'wb') as bmp_file:
         bmp_file.write(BitMapFileHeaderHandle)
         bmp_file.write(data)
-    print 'file "{}" created from clipboard image'.format(filename)
+    print('file "{}" created from clipboard image'.format(filename))
+    
+if __name__ == '__main__':
+    printScreen('test.png')

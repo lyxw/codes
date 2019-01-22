5cb4ea6d74c58f0f50d583f93b056480338c935a
lyxw <lyxw@users.noreply.github.com>
2019-01-22 16:21:05 +0800

    Create suanfa.py

---
+++ note/suanfa.py
@@ -0,0 +1,75 @@
+# coding=utf-8
+
+#插入排序
+def insert_sort(lists):
+    count = len(lists)
+    for i in range(1, count):
+        key = lists[i]
+        j = i - 1
+        while j >= 0:
+            if lists[j] > key:
+                lists[j + 1] = lists[j]
+                lists[j] = key
+            j -= 1
+    return lists
+
+#希尔排序
+def shell_sort(lists):
+    count = len(lists)
+    step = 2
+    group = count / step
+    while group > 0:
+        for i in range(0, group):
+            j = i + group
+            while j < count:
+                k = j - group
+                key = list[j]
+                while k >= 0:
+                    if lists[k] > key:
+                        lists[k + group] = lists[k]
+                        list[k] = key
+                    k -= group
+                j += group
+        group /= step
+    return lists
+
+#冒泡排序
+def bubble_sort(lists):
+    count = len(lists)
+    for i in range(0, count):
+        for j in range(i + 1, count):
+            if lists[i] > lists[j]:
+                lists[i], lists[j] = lists[j], lists[i]
+    return lists
+
+#快速排序
+def quick_sort(lists, left, right):
+    if left >= right:
+        return lists
+    key = lists[left]
+    low = left
+    high = right
+    while left < right:
+        while left < right and lists[right] >= key:
+            right -= 1
+        lists[left] = lists[right]
+        while left < right and lists[left] <= key:
+            left += 1
+        lists[right] = lists[left]
+    lists[right] = key
+    quick_sort(lists, low, left - 1)
+    quick_sort(lists, left + 1, high)  
+    return lists
+
+#直接选择排序
+def select_sort(lists):
+    count = len(lists)
+    for i in range(0, count):
+        min = i
+        for j in range(i + 1, count):
+            if lists[min] > lists[j]:
+                min=j
+        lists[min], lists[i] = lists[i], lists[min]
+    return lists
+
+#堆排序
